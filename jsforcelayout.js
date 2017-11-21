5ba3149eb26051d14b77a9323963a7af8b179971
kenshinee <kenshinee@users.noreply.github.com>
2017-11-21 14:41:08 +1000

    Update jsforcelayout.js

--- jsforcelayout.js
+++ jsforcelayout.js
@@ -195,6 +195,12 @@ JSForceLayout3 = function () {
     obj.resetAll();
 
 
+    String.prototype.hashCode = function () {
+        for (var ret = 0, i = 0, len = this.length; i < len; i++) {
+            ret = (31 * ret + this.charCodeAt(i)) << 0;
+        }
+        return ret;
+    };
 
     return obj;
 }
